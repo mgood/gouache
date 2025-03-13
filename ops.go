79ae5ae7ef2f923272037489fc150784ef204a45
Matt Good <29016+mgood@users.noreply.github.com>
2025-03-13 15:03:50 -0700

    ops refactor
    Pull out some common op computations to reduce code duplication.

---
+++ ops.go
@@ -0,0 +1,59 @@
+package gouache
+
+import "fmt"
+
+func (n Divert) GetDest(el Element, stack *CallFrame) (Element, *CallFrame) {
+	addr := n.Dest
+	if n.Var {
+		addrVar, ok := stack.GetVar(string(addr))
+		if !ok {
+			panic(fmt.Errorf("address variable %q not found", addr))
+		}
+		addr = addrVar.(DivertTargetValue).Dest
+	}
+	if n.Conditional {
+		var cond Value
+		cond, stack = stack.PopVal()
+		if !truthy(cond) {
+			return el.Next(), stack
+		}
+	}
+	if n.incTurnCount {
+		stack = stack.IncTurnCount()
+	}
+	dest := el.Find(addr)
+	if dest == nil {
+		panic(fmt.Errorf("divert target %q not found", n.Dest))
+	}
+	return dest, stack
+}
+
+func (n DupTop) Apply(stack *CallFrame) *CallFrame {
+	v, stack := stack.PopVal()
+	stack = stack.PushVal(v)
+	return stack.PushVal(v)
+}
+
+func (n GetVar) Apply(stack *CallFrame) *CallFrame {
+	val, ok := stack.GetVar(n.Name)
+	if !ok {
+		panic(fmt.Errorf("variable %q not found", n.Name))
+	}
+	return stack.PushVal(val)
+}
+
+func (n SetVar) Apply(stack *CallFrame) *CallFrame {
+	var val Value
+	val, stack = stack.PopVal()
+	if n.Reassign {
+		return stack.UpdateVar(n.Name, val)
+	}
+	return stack.WithGlobal(n.Name, val)
+}
+
+func (n SetTemp) Apply(stack *CallFrame) *CallFrame {
+	val, stack := stack.PopVal()
+	// TODO use "re" reassign flag to check that it's already
+	// been set?
+	return stack.WithLocal(n.Name, val)
+}
